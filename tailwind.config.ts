1fc386e9f97b3d40df72ebc56d12f2eccfa5c794
MasterHiei <ruhrg.lcx@outlook.com>
2024-09-13 17:20:16 +0800

    chore: update project folder structure

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,6 @@ import { TailwindConfig } from 'tailwindcss/tailwind-config'
 const config: TailwindConfig = {
   content: [
     './app/**/*.{js,ts,jsx,tsx,mdx}',
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
